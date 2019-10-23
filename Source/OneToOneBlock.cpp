f3da50b209411d87c4285fc10b62a88a6daa928b
Nicholas Corgan <n.corgan@gmail.com>
2019-10-22 19:58:32 -0500

    Merged OneToOneBlock and OneToOneBatchBlock

--- Source/OneToOneBlock.cpp
+++ Source/OneToOneBlock.cpp
@@ -10,6 +10,7 @@
 #include <arrayfire.h>
 
 #include <cassert>
+#include <cstring>
 #include <string>
 #include <typeinfo>
 
@@ -25,11 +26,12 @@ class OneToOneBlock: public ArrayFireBlock
         static Pothos::Block* makeFromOneType(
             const OneToOneFunc& func,
             const Pothos::DType& dtype,
-            const DTypeSupport& supportedTypes)
+            const DTypeSupport& supportedTypes,
+            size_t numChans)
         {
             validateDType(dtype, supportedTypes);
 
-            return new OneToOneBlock(func, dtype, dtype);
+            return new OneToOneBlock(func, dtype, dtype, numChans);
         }
 
         static Pothos::Block* makeFromTwoTypes(
@@ -37,7 +39,8 @@ class OneToOneBlock: public ArrayFireBlock
             const Pothos::DType& inputDType,
             const Pothos::DType& outputDType,
             const DTypeSupport& supportedInputTypes,
-            const DTypeSupport& supportedOutputTypes)
+            const DTypeSupport& supportedOutputTypes,
+            size_t numChans)
         {
             validateDType(inputDType, supportedInputTypes);
             validateDType(outputDType, supportedOutputTypes);
@@ -55,7 +58,7 @@ class OneToOneBlock: public ArrayFireBlock
                     inputDType);
             }
 
-            return new OneToOneBlock(func, inputDType, outputDType);
+            return new OneToOneBlock(func, inputDType, outputDType, numChans);
         }
 
         //
@@ -65,32 +68,81 @@ class OneToOneBlock: public ArrayFireBlock
         OneToOneBlock(
             const OneToOneFunc& func,
             const Pothos::DType& inputDType,
-            const Pothos::DType& outputDType
+            const Pothos::DType& outputDType,
+            size_t numChans
         ): ArrayFireBlock(),
            _func(func)
         {
-            this->setupInput(0, inputDType);
-            this->setupOutput(0, outputDType);
+            for(size_t chan = 0; chan < numChans; ++chan)
+            {
+                this->setupInput(chan, inputDType);
+                this->setupOutput(chan, outputDType);
+            }
         }
 
         virtual ~OneToOneBlock() {}
 
+        af::array getNumberedInputPortsAs2DAfArray()
+        {
+            // Assumptions:
+            //  * We've already checked that all buffers are non-empty.
+            //  * We only have numbered ports.
+            //  * All DTypes are the same.
+            const auto& inputs = this->inputs();
+            assert(!inputs.empty());
+
+            const auto dim0 = static_cast<dim_t>(inputs.size());
+            const auto dim1 = static_cast<dim_t>(this->workInfo().minElements);
+            const auto afDType = Pothos::Object(inputs[0]->dtype()).convert<::af_dtype>();
+            const size_t rowSizeBytes = inputs[0]->dtype().size() * dim1;
+
+            af::array ret(dim0, dim1, afDType);
+            for(dim_t row = 0; row < dim0; ++row)
+            {
+                auto arrayRow = ret.row(row);
+                std::memcpy(
+                    arrayRow.device<std::uint8_t>(),
+                    inputs[row]->buffer().as<const std::uint8_t*>(),
+                    rowSizeBytes);
+                arrayRow.unlock();
+
+                inputs[row]->consume(dim1);
+            }
+
+            return ret;
+        }
+
+        void post2DAfArrayToNumberedOutputPorts(const af::array& afArray)
+        {
+            const auto& outputs = this->outputs();
+            assert(outputs.size() == static_cast<size_t>(afArray.dims(0)));
+
+            for(size_t portIndex = 0; portIndex < outputs.size(); ++portIndex)
+            {
+                auto outputBuffer = Pothos::Object(afArray.row(portIndex))
+                                        .convert<Pothos::BufferChunk>();
+                outputs[portIndex]->postBuffer(std::move(outputBuffer));
+            }
+        }
+
         void work() override
         {
-            const size_t elems = this->workInfo().minAllElements;
-
-            if(0 == elems)
+            if(0 == this->workInfo().minElements)
             {
                 return;
             }
 
-            auto inputAfArray = this->getInputPortAsAfArray(0);
-            assert(elems == inputAfArray.elements());
+            auto afInput = getNumberedInputPortsAs2DAfArray();
+            const auto dim0 = afInput.dims(0);
+            const auto dim1 = afInput.dims(1);
 
-            auto outputAfArray = _func(inputAfArray);
+            af::array afOutput(dim0, dim1, afInput.type());
+            gfor(af::seq row, dim0)
+            {
+                afOutput(row) = _func(afInput(row));
+            }
 
-            this->input(0)->consume(elems);
-            this->postAfArray(0, outputAfArray);
+            post2DAfArrayToNumberedOutputPorts(afOutput);
         }
 
     private:
