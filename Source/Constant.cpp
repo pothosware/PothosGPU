bdac2fec731fab26573cfcd91e2d4cf8282dee22
Nicholas Corgan <n.corgan@gmail.com>
2019-11-16 11:31:02 -0600

    Constant: added PothosDoc comment, fixed complex support

--- Source/Constant.cpp
+++ Source/Constant.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 #include "ArrayFireBlock.hpp"
+#include "Utility.hpp"
 
 #include <Pothos/Exception.hpp>
 #include <Pothos/Framework.hpp>
@@ -17,16 +18,16 @@ class Constant: public ArrayFireBlock
 {
     public:
 
+        using Class = Constant<T>;
+
         Constant(T constant):
             ArrayFireBlock(),
-            _constant(constant),
-            _afDType(Pothos::Object(Constant<T>::dtype).convert<af::dtype>())
+            _constant(PothosToAF<T>::to(constant)),
+            _afDType(Pothos::Object(Class::dtype).convert<af::dtype>())
         {
-            using Class = Constant<T>;
-
             this->registerCall(this, POTHOS_FCN_TUPLE(Class, getConstant));
             this->registerCall(this, POTHOS_FCN_TUPLE(Class, setConstant));
-            this->setupOutput(0, Constant<T>::dtype);
+            this->setupOutput(0, Class::dtype);
         }
 
         virtual ~Constant() {}
@@ -44,32 +45,34 @@ class Constant: public ArrayFireBlock
 
         T getConstant() const
         {
-            return _constant;
+            return PothosToAF<T>::from(_constant);
         }
 
         void setConstant(const T& constant)
         {
-            _constant = constant;
+            _constant = PothosToAF<T>::to(constant);
         }
 
     private:
 
         static const Pothos::DType dtype;
 
-        T _constant;
+        typename PothosToAF<T>::type _constant;
         af::dtype _afDType;
 };
 
 template <typename T>
 const Pothos::DType Constant<T>::dtype(typeid(T));
 
-static Pothos::Block* constantFactory(const Pothos::DType& dtype)
+static Pothos::Block* constantFactory(
+    const Pothos::DType& dtype,
+    const Pothos::Object& constant)
 {
     #define ifTypeDeclareFactory(T) \
         if(Pothos::DType::fromDType(dtype, 1) == Pothos::DType(typeid(T))) \
-            return new Constant<T>(T(0));
+            return new Constant<T>(constant.convert<T>());
 
-    // ArrayFire has no implementation for std::int8_t or either complex type.
+    // ArrayFire has no implementation for std::int8_t.
     ifTypeDeclareFactory(std::int16_t)
     ifTypeDeclareFactory(std::int32_t)
     ifTypeDeclareFactory(std::int64_t)
@@ -79,12 +82,41 @@ static Pothos::Block* constantFactory(const Pothos::DType& dtype)
     ifTypeDeclareFactory(std::uint64_t)
     ifTypeDeclareFactory(float)
     ifTypeDeclareFactory(double)
+    ifTypeDeclareFactory(std::complex<float>)
+    ifTypeDeclareFactory(std::complex<double>)
 
     throw Pothos::InvalidArgumentException(
               "Unsupported type",
               dtype.name());
 }
 
+/*
+ * |PothosDoc Constant
+ *
+ * Calls <b>af::constant</b> on all inputs. to fill buffers with a given
+ * constant. This is potentially accelerated using one of the following
+ * implementations by priority (based on availability of hardware
+ * and underlying libraries).
+ * <ol>
+ * <li>CUDA (if GPU present)</li>
+ * <li>OpenCL (if GPU present)</li>
+ * <li>Standard C++ (if no GPU present)</li>
+ * </ol>
+ *
+ * |category /ArrayFire/Data
+ * |keywords data constant
+ * |factory /arrayfire/data/constant(dtype,constant)
+ *
+ * |param dtype(Data Type) The block data type.
+ * |widget DTypeChooser(int=1,uint=1,float=1,cfloat=1)
+ * |default "float64"
+ * |preview enable
+ *
+ * |param constant(Constant) Which constant to fill the buffer with.
+ * |widget LineEdit()
+ * |default 0
+ * |preview enable
+ */
 static Pothos::BlockRegistry registerConstant(
     "/arrayfire/data/constant",
     Pothos::Callable(&constantFactory));
