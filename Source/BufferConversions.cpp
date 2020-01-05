d86b9faf84fd618455a20dde2c5044a394c9b1bb
Nicholas Corgan <n.corgan@gmail.com>
2020-01-04 18:46:50 -0600

    af::array(_proxy) -> Pothos::BufferChunk: point BufferChunk at device memory

--- Source/BufferConversions.cpp
+++ Source/BufferConversions.cpp
@@ -1,8 +1,13 @@
 // Copyright (c) 2019 Nicholas Corgan
 // SPDX-License-Identifier: BSD-3-Clause
 
+#include <Pothos/Exception.hpp>
 #include <Pothos/Framework.hpp>
 #include <Pothos/Plugin.hpp>
+#include <Pothos/Util/TypeInfo.hpp>
+
+#include <Poco/Format.h>
+#include <Poco/NumberFormatter.h>
 
 #include <arrayfire.h>
 
@@ -11,11 +16,25 @@
 template <typename AfArrayType>
 static Pothos::BufferChunk afArrayTypeToBufferChunk(const AfArrayType& afArray)
 {
-    const auto dtype = Pothos::Object(afArray.type()).convert<Pothos::DType>();
-    const auto elements = afArray.elements();
+    auto afArraySPtr = std::make_shared<af::array>(afArray);
+    const size_t address = reinterpret_cast<size_t>(afArraySPtr->template device<std::uint8_t>());
+    const size_t bytes = afArraySPtr->bytes();
 
-    Pothos::BufferChunk bufferChunk(dtype, elements);
-    afArray.host(reinterpret_cast<void*>(bufferChunk.address));
+    Pothos::SharedBuffer sharedBuff(address, bytes, afArraySPtr);
+    Pothos::BufferChunk bufferChunk(sharedBuff);
+    bufferChunk.dtype = Pothos::Object(afArray.type()).convert<Pothos::DType>();
+    if(bufferChunk.elements() != static_cast<size_t>(afArraySPtr->elements()))
+    {
+        throw Pothos::AssertionViolationException(
+                  Poco::format(
+                      "Element count doesn't match in %s conversion to Pothos::BufferChunk",
+                      Pothos::Util::typeInfoToString(typeid(AfArrayType))),
+                  Poco::format(
+                      "%s -> %s",
+                      Poco::NumberFormatter::format(afArraySPtr->elements()),
+                      bufferChunk.elements()));
+    }
+
     return bufferChunk;
 }
 
