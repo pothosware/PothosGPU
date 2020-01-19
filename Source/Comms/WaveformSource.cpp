bd4dff558a1460462e838d457d8932c3d428cce0
Nicholas Corgan <n.corgan@gmail.com>
2020-01-19 14:51:45 -0600

    Progress on porting over WaveformSource

--- Source/Comms/WaveformSource.cpp
+++ Source/Comms/WaveformSource.cpp
@@ -4,8 +4,10 @@
 
 #include "ArrayFireBlock.hpp"
 #include "Functions.hpp"
+#include "Utility.hpp"
 
 #include <Pothos/Framework.hpp>
+#include <Pothos/Object.hpp>
 
 #include <cmath>
 #include <cstdint>
@@ -74,11 +76,20 @@ template <typename Type>
 class WaveformSource : public ArrayFireBlock
 {
 public:
+    using AFType = typename PothosToAF<Type>::type;
+
     WaveformSource(const std::string& device):
         ArrayFireBlock(device),
         _index(0), _step(0), _mask(0),
-        _rate(1.0), _freq(0.0), _res(0.0),
-        _offset(0.0), _scalar(1.0),
+        __rate(1.0), __freq(0.0), __res(0.0),
+        __offset(0.0), __scalar(1.0),
+        _rate(PothosToAF<double>::to(1.0)),
+        _freq(PothosToAF<double>::to(0.0)),
+        _res(PothosToAF<double>::to(0.0)),
+        _offset(PothosToAF<std::complex<double>>::to(0.0)),
+        _scalar(PothosToAF<std::complex<double>>::to(1.0)),
+        _afTable(),
+        _afDType(Pothos::Object(Pothos::DType(typeid(Type))).convert<af::dtype>()),
         _wave("CONST")
     {
         this->setupOutput(0, typeid(Type));
@@ -107,7 +118,7 @@ public:
         Type *out = outPort->buffer();
         for (size_t i = 0; i < outPort->elements(); i++)
         {
-            out[i] = _table[_index & _mask];
+            out[i] = __table[_index & _mask];
             _index += _step;
         }
         outPort->produce(outPort->elements());
@@ -126,57 +137,57 @@ public:
 
     void setOffset(const std::complex<double> &offset)
     {
-        _offset = offset;
+        _offset = PothosToAF<std::complex<double>>::to(offset);
         this->updateTable();
     }
 
     std::complex<double> getOffset(void)
     {
-        return _offset;
+        return PothosToAF<std::complex<double>>::from(_offset);
     }
 
     void setAmplitude(const std::complex<double> &scalar)
     {
-        _scalar = scalar;
+        _scalar = PothosToAF<std::complex<double>>::to(scalar);
         this->updateTable();
     }
 
     std::complex<double> getAmplitude(void)
     {
-        return _scalar;
+        return PothosToAF<std::complex<double>>::from(_scalar);
     }
 
     void setFrequency(const double &freq)
     {
-        _freq = freq;
+        _freq = PothosToAF<double>::to(freq);
         this->updateTable();
     }
 
     double getFrequency(void)
     {
-        return _freq;
+        return PothosToAF<double>::from(_freq);
     }
 
     void setSampleRate(const double &rate)
     {
-        _rate = rate;
+        _rate = PothosToAF<double>::to(rate);
         this->updateTable();
     }
 
     double getSampleRate(void)
     {
-        return _rate;
+        return PothosToAF<double>::from(_rate);
     }
 
     void setResolution(const double &res)
     {
-        _res = res;
+        _res = PothosToAF<double>::to(res);
         this->updateTable();
     }
 
     double getResolution(void)
     {
-        return _res;
+        return PothosToAF<double>::from(_res);
     }
 
 private:
@@ -187,7 +198,7 @@ private:
 
         //This fraction (of a period) is used to determine table size efficacy.
         //When specified, use the resolution, otherwise the user's frequency.
-        const auto frac = ((_res == 0.0)?_freq:_res)/_rate;
+        const auto frac = ((__res == 0.0)?__freq:__res)/__rate;
 
         //loop for a table size that meets the minimum step
         size_t numEntries = defaultWaveTableSize;
@@ -196,7 +207,7 @@ private:
             const auto delta = std::llround(frac*numEntries);
             if (frac == 0.0) break;
             if (size_t(std::abs(delta)) >= minimumTableStepSize) break;
-            if (numEntries*2 > _table.max_size()) break;
+            if (numEntries*2 > __table.max_size()) break;
             if (numEntries*2 > maxWaveTableSize) break;
             numEntries *= 2;
         }
@@ -205,73 +216,130 @@ private:
         _mask = numEntries-1;
 
         //update step: given ratio and table size
-        _step = size_t(std::llround((_freq/_rate)*numEntries));
-        if (_step == 0 and _freq != 0.0)
+        _step = size_t(std::llround((__freq/__rate)*numEntries));
+        if (_step == 0 and __freq != 0.0)
         {
             throw Pothos::InvalidArgumentException("WaveformSource::updateTable()", "step size not achievable");
         }
 
         //resize the table for the new number of entries
-        _table.resize(numEntries);
+        __table.resize(numEntries);
 
         if (_wave == "CONST")
         {
-            for (size_t i = 0; i < _table.size(); i++)
-            {
-                this->setElem(_table[i], 1.0);
-            }
+            /*
+             * for (size_t i = 0; i < __table.size(); i++)
+             * {
+             *     this->setElem(__table[i], 1.0);
+             * }
+             */
+            _afTable = af::range(static_cast<dim_t>(numEntries)).as(_afDType);
         }
         else if (_wave == "SINE")
         {
-            for (size_t i = 0; i < _table.size(); i++){
-                this->setElem(_table[i], std::polar(1.0, 2*M_PI*i/_table.size()));
-            }
+            /*
+             * for (size_t i = 0; i < __table.size(); i++){
+             *     this->setElem(__table[i], std::polar(1.0, 2*M_PI*i/__table.size()));
+             * }
+             */
+            auto rho = af::identity(static_cast<dim_t>(numEntries)).as(::f64);
+
+            auto theta = af::range(static_cast<dim_t>(numEntries)).as(::f64);
+            theta *= 2.0;
+            theta *= af::Pi;
+            theta /= static_cast<double>(numEntries);
+
+            _afTable = polarToComplex(rho, theta);
         }
         else if (_wave == "RAMP")
         {
-            for (size_t i = 0; i < _table.size(); i++)
-            {
-                const size_t q = (i+(3*_table.size())/4)%_table.size();
-                this->setElem(_table[i], std::complex<double>(
-                    2.0*i/(_table.size()-1) - 1.0,
-                    2.0*q/(_table.size()-1) - 1.0
-                ));
-            }
+            /*
+             * for (size_t i = 0; i < __table.size(); i++)
+             * {
+             *     const size_t q = (i+(3*__table.size())/4)%__table.size();
+             *     this->setElem(__table[i], std::complex<double>(
+             *         2.0*i/(__table.size()-1) - 1.0,
+             *         2.0*q/(__table.size()-1) - 1.0
+             *     ));
+             * }
+             */
+            _afTable = af::complex(
+                           (I(numEntries) * 2.0 / static_cast<double>(numEntries-1)) - 1.0,
+                           (Q(numEntries) * 2.0 / static_cast<double>(numEntries-1)) - 1.0);
         }
         else if (_wave == "SQUARE")
         {
-            for (size_t i = 0; i < _table.size(); i++)
-            {
-                const size_t q = (i+(3*_table.size())/4)%_table.size();
-                this->setElem(_table[i], std::complex<double>(
-                    (i < _table.size()/2)? 0.0 : 1.0,
-                    (q < _table.size()/2)? 0.0 : 1.0
-                ));
-            }
+            /*
+             * for (size_t i = 0; i < __table.size(); i++)
+             * {
+             *     const size_t q = (i+(3*__table.size())/4)%__table.size();
+             *     this->setElem(__table[i], std::complex<double>(
+             *         (i < __table.size()/2)? 0.0 : 1.0,
+             *         (q < __table.size()/2)? 0.0 : 1.0
+             *     ));
+             * }
+             */
+
+            // Note: comparison operators return an int8 array of 0 or 1.
+            _afTable = af::complex(
+                           (I(numEntries) >= (numEntries/2)).as(::f64),
+                           (Q(numEntries) >= (numEntries/2)).as(::f64));
         }
         else throw Pothos::InvalidArgumentException("WaveformSource::setWaveform("+_wave+")", "unknown waveform setting");
+
+        _afTable = applyScalarAndOffset(_afTable);
+    }
+
+    af::array I(size_t tableSize)
+    {
+        return af::identity(static_cast<dim_t>(tableSize)).as(::f64);
+    }
+
+    af::array Q(size_t tableSize)
+    {
+        /*
+         * for (size_t i = 0; i < __table.size(); i++)
+         * {
+         *     const size_t q = (i+(3*__table.size())/4)%__table.size();
+         * }
+         */
+        return ((I(tableSize) + (3 * tableSize) / 4) % tableSize).as(::f64);
+    }
+
+    af::array applyScalarAndOffset(const af::array& afArray)
+    {
+        af::array ret = ((afArray * _scalar) + _offset);
+        if(afArray.iscomplex()) ret = af::real(ret);
+
+        return ret.as(_afDType);
     }
 
     template <typename T>
     void setElem(T &out, const std::complex<double> &val)
     {
-        out = Type((_scalar * val + _offset).real());
+        out = Type((__scalar * val + __offset).real());
     }
 
     template <typename T>
     void setElem(std::complex<T> &out, const std::complex<double> &val)
     {
-        out = Type(_scalar * val + _offset);
+        out = Type(__scalar * val + __offset);
     }
 
     size_t _index;
     size_t _step;
     size_t _mask;
-    double _rate;
-    double _freq;
-    double _res;
-    std::vector<Type> _table;
-    std::complex<double> _offset, _scalar;
+    double __rate;
+    double __freq;
+    double __res;
+    std::vector<Type> __table;
+    std::complex<double> __offset, __scalar;
+
+    PothosToAF<double>::type _rate, _freq, _res;
+    PothosToAF<std::complex<double>>::type _offset, _scalar;
+    af::array _afTable;
+    af::dtype _afDType;
+
     std::string _wave;
 };
 
@@ -279,14 +347,14 @@ private:
  * registration
  **********************************************************************/
 static Pothos::Block *waveformSourceFactory(
-    const std::string& device,
-    const Pothos::DType& dtype)
+    const std::string &device,
+    const Pothos::DType &dtype)
 {
     #define ifTypeDeclareFactory(type) \
         if (dtype == Pothos::DType(typeid(type))) return new WaveformSource<type>(device); \
         if (dtype == Pothos::DType(typeid(std::complex<type>))) return new WaveformSource<std::complex<type>>(device);
-    ifTypeDeclareFactory(double);
     ifTypeDeclareFactory(float);
+    ifTypeDeclareFactory(double);
     throw Pothos::InvalidArgumentException("waveformSourceFactory("+dtype.toString()+")", "unsupported type");
 }
 
