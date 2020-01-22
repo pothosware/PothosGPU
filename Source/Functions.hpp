ec1eff6f6f95ac02d9300642066ce3b53042fa1d
Nicholas Corgan <n.corgan@gmail.com>
2020-01-21 20:23:05 -0600

    Added LogN, with optimized cases for log2 and log10

--- Source/Functions.hpp
+++ Source/Functions.hpp
@@ -10,3 +10,8 @@ inline af::array polarToComplex(const af::array& rho, const af::array& theta)
 {
     return af::complex((rho * af::cos(theta)), (rho * af::sin(theta)));
 };
+
+inline af::array logN(const af::array& afArray, double base)
+{
+    return af::log(afArray) / af::log(af::constant(base, afArray.elements()));
+}
