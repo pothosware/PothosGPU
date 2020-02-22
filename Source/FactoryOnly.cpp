83d6b9618fac82ab25c9a29e3d3246d9d0501343
Nicholas Corgan <n.corgan@gmail.com>
2020-02-21 18:05:40 -0600

    Added more manual blocks

--- Source/FactoryOnly.cpp
+++ Source/FactoryOnly.cpp
@@ -11,21 +11,75 @@
 // TODO: add tests
 
 static Pothos::BlockRegistry registerSec(
-    "/arrayfire/arith/sec",
+    "/arrayfire/trig/sec",
     Pothos::Callable(&OneToOneBlock::makeFromOneType)
         .bind(sec, 1)
         .bind<DTypeSupport>({false,false,true,false}, 3));
 
 static Pothos::BlockRegistry registerCsc(
-    "/arrayfire/arith/csc",
+    "/arrayfire/trig/csc",
     Pothos::Callable(&OneToOneBlock::makeFromOneType)
         .bind(csc, 1)
         .bind<DTypeSupport>({false,false,true,false}, 3));
 
 static Pothos::BlockRegistry registerCot(
-    "/arrayfire/arith/cot",
+    "/arrayfire/trig/cot",
     Pothos::Callable(&OneToOneBlock::makeFromOneType)
         .bind(cot, 1)
+        .bind<DTypeSupport>({false,false,true,false}, 3));
+
+static Pothos::BlockRegistry registerASec(
+    "/arrayfire/trig/asec",
+    Pothos::Callable(&OneToOneBlock::makeFromOneType)
+        .bind(asec, 1)
+        .bind<DTypeSupport>({false,false,true,false}, 3));
+
+static Pothos::BlockRegistry registerACsc(
+    "/arrayfire/trig/acsc",
+    Pothos::Callable(&OneToOneBlock::makeFromOneType)
+        .bind(acsc, 1)
+        .bind<DTypeSupport>({false,false,true,false}, 3));
+
+static Pothos::BlockRegistry registerACot(
+    "/arrayfire/trig/acot",
+    Pothos::Callable(&OneToOneBlock::makeFromOneType)
+        .bind(acot, 1)
+        .bind<DTypeSupport>({false,false,true,false}, 3));
+
+static Pothos::BlockRegistry registerSecH(
+    "/arrayfire/trig/sech",
+    Pothos::Callable(&OneToOneBlock::makeFromOneType)
+        .bind(sech, 1)
+        .bind<DTypeSupport>({false,false,true,false}, 3));
+
+static Pothos::BlockRegistry registerCscH(
+    "/arrayfire/trig/csch",
+    Pothos::Callable(&OneToOneBlock::makeFromOneType)
+        .bind(csch, 1)
+        .bind<DTypeSupport>({false,false,true,false}, 3));
+
+static Pothos::BlockRegistry registerCotH(
+    "/arrayfire/trig/coth",
+    Pothos::Callable(&OneToOneBlock::makeFromOneType)
+        .bind(coth, 1)
+        .bind<DTypeSupport>({false,false,true,false}, 3));
+
+static Pothos::BlockRegistry registerASecH(
+    "/arrayfire/trig/asech",
+    Pothos::Callable(&OneToOneBlock::makeFromOneType)
+        .bind(asech, 1)
+        .bind<DTypeSupport>({false,false,true,false}, 3));
+
+static Pothos::BlockRegistry registerACscH(
+    "/arrayfire/trig/acsch",
+    Pothos::Callable(&OneToOneBlock::makeFromOneType)
+        .bind(acsch, 1)
+        .bind<DTypeSupport>({false,false,true,false}, 3));
+
+static Pothos::BlockRegistry registerACotH(
+    "/arrayfire/trig/acoth",
+    Pothos::Callable(&OneToOneBlock::makeFromOneType)
+        .bind(acoth, 1)
         .bind<DTypeSupport>({false,false,true,false}, 3));
 
 static Pothos::BlockRegistry registerPolarToComplex(
@@ -33,3 +87,10 @@ static Pothos::BlockRegistry registerPolarToComplex(
     Pothos::Callable(&TwoToOneBlock::makeFloatToComplex)
         .bind(polarToComplex, 1)
         .bind(false, 3));
+
+static Pothos::BlockRegistry registerHypot(
+    "/arrayfire/arith/hypotenuse",
+    Pothos::Callable(&TwoToOneBlock::makeFromOneType)
+        .bind(hypotenuse, 1)
+        .bind<DTypeSupport>({false,false,true,false}, 3)
+        .bind(true, 4 /*allowZeroInBuffer1*/));
--- Source/Functions.hpp
+++ Source/Functions.hpp
@@ -18,15 +18,65 @@ inline af::array logN(const af::array& afArray, double base)
 
 inline af::array sec(const af::array& afArray)
 {
-    return af::constant(1.0, afArray.elements(), afArray.type()) / af::cos(afArray);
+    return 1.0 / af::cos(afArray);
 }
 
 inline af::array csc(const af::array& afArray)
 {
-    return af::constant(1.0, afArray.elements(), afArray.type()) / af::sin(afArray);
+    return 1.0 / af::sin(afArray);
 }
 
 inline af::array cot(const af::array& afArray)
 {
-    return af::constant(1.0, afArray.elements(), afArray.type()) / af::tan(afArray);
+    return 1.0 / af::tan(afArray);
+}
+
+inline af::array asec(const af::array& afArray)
+{
+    return af::acos(1.0 / afArray);
+}
+
+inline af::array acsc(const af::array& afArray)
+{
+    return af::asin(1.0 / afArray);
+}
+
+inline af::array acot(const af::array& afArray)
+{
+    return af::atan(1.0 / afArray);
+}
+
+inline af::array sech(const af::array& afArray)
+{
+    return 1.0 / af::cosh(afArray);
+}
+
+inline af::array csch(const af::array& afArray)
+{
+    return 1.0 / af::sinh(afArray);
+}
+
+inline af::array coth(const af::array& afArray)
+{
+    return 1.0 / af::tanh(afArray);
+}
+
+inline af::array asech(const af::array& afArray)
+{
+    return af::acosh(1.0 / afArray);
+}
+
+inline af::array acsch(const af::array& afArray)
+{
+    return af::asinh(1.0 / afArray);
+}
+
+inline af::array acoth(const af::array& afArray)
+{
+    return af::atanh(1.0 / afArray);
+}
+
+inline af::array hypotenuse(const af::array& afArray0, const af::array& afArray1)
+{
+    return af::sqrt(af::pow2(afArray0) + af::pow2(afArray1));
 }
