d7d142bd47543a3a2e65e597763b3670228732d3
Nicholas Corgan <n.corgan@gmail.com>
2020-02-27 18:14:38 -0600

    Array and scalar blocks appear in PothosFlow

--- Source/ArrayOpBlock.cpp
+++ Source/ArrayOpBlock.cpp
@@ -17,12 +17,16 @@
 #define NToOneLambda(op) \
     [](const af::array& a, const af::array& b){return a op b;}
 
-#define IfOpThenReducedBlock(op, func) \
-    if(#op == operation) \
+#define IfOpThenReducedBlock(opStr, func) \
+    if(opStr == operation) \
         return new ReducedBlock(device, &func, dtype, dtype, numChans);
 
-#define IfOpThenNToOneBlock(op) \
-    if(#op == operation) \
+#define IfOpThenReducedBlockInt8(opStr, func) \
+    if(opStr == operation) \
+        return new ReducedBlock(device, &func, dtype, "int8", numChans);
+
+#define IfOpThenNToOneBlock(op, opStr) \
+    if(opStr == operation) \
         return new NToOneBlock(device, NToOneLambda(op), dtype, numChans);
 
 static Pothos::Block* makeArrayArithmetic(
@@ -31,10 +35,10 @@ static Pothos::Block* makeArrayArithmetic(
     const Pothos::DType& dtype,
     size_t numChans)
 {
-    IfOpThenReducedBlock(+, af::sum)
-    else IfOpThenNToOneBlock(-)
-    else IfOpThenReducedBlock(*, af::product)
-    else IfOpThenNToOneBlock(/)
+    IfOpThenReducedBlock("ADD", af::sum)
+    else IfOpThenNToOneBlock(-, "SUBTRACT")
+    else IfOpThenReducedBlock("MULTIPLY", af::product)
+    else IfOpThenNToOneBlock(/, "DIVIDE")
 
     throw Pothos::InvalidArgumentException("Invalid operation", operation);
 }
@@ -45,11 +49,16 @@ static Pothos::Block* makeArrayBitwise(
     const Pothos::DType& dtype,
     size_t numChans)
 {
-    IfOpThenNToOneBlock(&)
-    else IfOpThenNToOneBlock(|)
-    else IfOpThenNToOneBlock(^)
+    if(isDTypeAnyInt(dtype))
+    {
+        IfOpThenNToOneBlock(&, "AND")
+        else IfOpThenNToOneBlock(|, "OR")
+        else IfOpThenNToOneBlock(^, "XOR")
 
-    throw Pothos::InvalidArgumentException("Invalid operation", operation);
+        throw Pothos::InvalidArgumentException("Invalid operation", operation);
+    }
+
+    throw Pothos::InvalidArgumentException("Invalid type", dtype.name());
 }
 
 static Pothos::Block* makeArrayLogical(
@@ -58,22 +67,125 @@ static Pothos::Block* makeArrayLogical(
     const Pothos::DType& dtype,
     size_t numChans)
 {
-    IfOpThenReducedBlock(&&, af::allTrue)
-    else IfOpThenReducedBlock(||, af::anyTrue)
+    if(isDTypeAnyInt(dtype))
+    {
+        IfOpThenReducedBlockInt8("AND", af::allTrue)
+        else IfOpThenReducedBlockInt8("OR", af::anyTrue)
 
-    throw Pothos::InvalidArgumentException("Invalid operation", operation);
+        throw Pothos::InvalidArgumentException("Invalid operation", operation);
+    }
+
+    throw Pothos::InvalidArgumentException("Invalid type", dtype.name());
 }
 
 // TODO: TwoToOne: comparators, << >>
 
+//
+// Block registries
+//
+
+/*
+ * |PothosDoc Arithmetic
+ *
+ * Perform the specified arithmetic operation on all given inputs, resulting
+ * in a single output stream.
+ *
+ * |category /ArrayFire/Array
+ * |keywords array add subtract multiply divide modulus
+ * |factory /arrayfire/array/arithmetic(device,operation,dtype,nchans)
+ *
+ * |param device[Device] ArrayFire device to use.
+ * |default "Auto"
+ *
+ * |param operation[Operation] The arithmetic operation to perform.
+ * |widget ComboBox(editable=false)
+ * |option [Add] "ADD"
+ * |option [Subtract] "SUBTRACT"
+ * |option [Multiply] "MULTIPLY"
+ * |option [Divide] "DIVIDE"
+ * |default "ADD"
+ * |preview enable
+ *
+ * |param dtype[Data Type] The output's data type.
+ * |widget DTypeChooser(int16=1,int32=1,int64=1,uint=1,float=1,cfloat=1)
+ * |default "float64"
+ * |preview disable
+ *
+ * |param nchans(Num Channels) The number of arithmetic inputs.
+ * |widget SpinBox(minimum=2)
+ * |default 2
+ * |preview disable
+ */
 static Pothos::BlockRegistry registerArrayArithmetic(
     "/arrayfire/array/arithmetic",
     Pothos::Callable(&makeArrayArithmetic));
 
+/*
+ * |PothosDoc Bitwise
+ *
+ * Perform the specified bitwise operation on all given inputs, resulting
+ * in a single output stream.
+ *
+ * |category /ArrayFire/Array
+ * |keywords array and or xor
+ * |factory /arrayfire/array/bitwise(device,operation,dtype,nchans)
+ *
+ * |param device[Device] ArrayFire device to use.
+ * |default "Auto"
+ *
+ * |param operation[Operation] The arithmetic operation to perform.
+ * |widget ComboBox(editable=false)
+ * |option [And] "AND"
+ * |option [Or] "OR"
+ * |option [XOR] "XOR"
+ * |default "AND"
+ * |preview enable
+ *
+ * |param dtype[Data Type] The output's data type.
+ * |widget DTypeChooser(int16=1,int32=1,int64=1,uint=1)
+ * |default "uint64"
+ * |preview disable
+ *
+ * |param nchans(Num Channels) The number of arithmetic inputs.
+ * |widget SpinBox(minimum=2)
+ * |default 2
+ * |preview disable
+ */
 static Pothos::BlockRegistry registerArrayBitwise(
     "/arrayfire/array/bitwise",
     Pothos::Callable(&makeArrayBitwise));
 
+/*
+ * |PothosDoc Logical
+ *
+ * Perform the specified logical operation on all given inputs, resulting
+ * in a single output stream of type <b>int8</b>, where <b>0</b> corresponds
+ * to false and <b>1</b> corresponds to true.
+ *
+ * |category /ArrayFire/Array
+ * |keywords array and or xor
+ * |factory /arrayfire/array/logical(device,operation,dtype,nchans)
+ *
+ * |param device[Device] ArrayFire device to use.
+ * |default "Auto"
+ *
+ * |param operation[Operation] The arithmetic operation to perform.
+ * |widget ComboBox(editable=false)
+ * |option [And] "AND"
+ * |option [Or] "OR"
+ * |default "AND"
+ * |preview enable
+ *
+ * |param dtype[Data Type] The output's data type.
+ * |widget DTypeChooser(int16=1,int32=1,int64=1,uint=1)
+ * |default "uint64"
+ * |preview disable
+ *
+ * |param nchans(Num Channels) The number of arithmetic inputs.
+ * |widget SpinBox(minimum=2)
+ * |default 2
+ * |preview disable
+ */
 static Pothos::BlockRegistry registerArrayLogical(
     "/arrayfire/array/logical",
     Pothos::Callable(&makeArrayLogical));
--- Source/ScalarOpBlock.cpp
+++ Source/ScalarOpBlock.cpp
@@ -30,6 +30,7 @@ class ScalarOpBlock: public OneToOneBlock
             const std::string& device,
             const AfArrayScalarOp<T>& func,
             const Pothos::DType& dtype,
+            const Pothos::DType& outputDType,
             const T& scalar,
             bool allowZeroOperand
         ):
@@ -37,20 +38,21 @@ class ScalarOpBlock: public OneToOneBlock
             device,
             Pothos::Callable(func),
             dtype,
-            dtype),
+            outputDType),
         _allowZeroOperand(allowZeroOperand)
         {
-            this->registerCall(this, POTHOS_FCN_TUPLE(Class, getScalar));
+            this->registerCall(this, POTHOS_FCN_TUPLE(Class, scalar));
             this->registerCall(this, POTHOS_FCN_TUPLE(Class, setScalar));
 
-            this->registerProbe("getScalar", "scalarChanged", "setScalar");
+            this->registerProbe("scalar");
+            this->registerSignal("scalarChanged");
 
             setScalar(scalar);
         }
 
         virtual ~ScalarOpBlock() = default;
 
-        T getScalar() const
+        T scalar() const
         {
             return PothosToAF<T>::from(_scalar);
         }
@@ -90,9 +92,9 @@ enum class ScalarBlockType
     LOGICAL
 };
 
-#define IfTypeThenLambda(op, callerOp, cType, dest) \
-    if(#op == callerOp) \
-        dest = [](const af::array& a, const PothosToAF<cType>::type& b){return a op b;};
+#define IfTypeThenLambda(op, opStr, callerOp, cType, dest) \
+    if(opStr == callerOp) \
+        dest = [](const af::array& a, const PothosToAF<cType>::type& b){return (a op b);};
 
 static Pothos::Block* makeScalarOpBlock(
     ScalarBlockType blockType,
@@ -102,6 +104,7 @@ static Pothos::Block* makeScalarOpBlock(
     const Pothos::Object& scalarObject)
 {
     const bool allowZeroScalar = ("/" != operation) && ("%" != operation);
+    static const Pothos::DType Int8DType("int8");
 
     #define IfTypeDeclareFactory(cType) \
         if(Pothos::DType::fromDType(dtype, 1) == Pothos::DType(typeid(cType))) \
@@ -110,33 +113,33 @@ static Pothos::Block* makeScalarOpBlock(
             switch(blockType) \
             { \
                 case ScalarBlockType::ARITHMETIC: \
-                    IfTypeThenLambda(+, operation, cType, func) \
-                    else IfTypeThenLambda(-, operation, cType, func) \
-                    else IfTypeThenLambda(*, operation, cType, func) \
-                    else IfTypeThenLambda(/, operation, cType, func) \
-                    else IfTypeThenLambda(%, operation, cType, func) \
+                    IfTypeThenLambda(+, "ADD", operation, cType, func) \
+                    else IfTypeThenLambda(-, "SUBTRACT", operation, cType, func) \
+                    else IfTypeThenLambda(*, "MULTIPLY", operation, cType, func) \
+                    else IfTypeThenLambda(/, "DIVIDE", operation, cType, func) \
+                    else IfTypeThenLambda(%, "MODULUS", operation, cType, func) \
                     else throw Pothos::InvalidArgumentException("Invalid operation", operation); \
                     break; \
                 case ScalarBlockType::COMPARATOR: \
-                    IfTypeThenLambda(>, operation, cType, func) \
-                    else IfTypeThenLambda(>=, operation, cType, func) \
-                    else IfTypeThenLambda(<, operation, cType, func) \
-                    else IfTypeThenLambda(<=, operation, cType, func) \
-                    else IfTypeThenLambda(==, operation, cType, func) \
-                    else IfTypeThenLambda(!=, operation, cType, func) \
+                    IfTypeThenLambda(>, ">", operation, cType, func) \
+                    else IfTypeThenLambda(>=, ">=", operation, cType, func) \
+                    else IfTypeThenLambda(<, "<", operation, cType, func) \
+                    else IfTypeThenLambda(<=, "<=", operation, cType, func) \
+                    else IfTypeThenLambda(==, "==", operation, cType, func) \
+                    else IfTypeThenLambda(!=, "!=", operation, cType, func) \
                     else throw Pothos::InvalidArgumentException("Invalid operation", operation); \
                     break; \
                 case ScalarBlockType::BITWISE: \
-                    IfTypeThenLambda(&, operation, cType, func) \
-                    else IfTypeThenLambda(|, operation, cType, func) \
-                    else IfTypeThenLambda(^, operation, cType, func) \
-                    else IfTypeThenLambda(<<, operation, cType, func) \
-                    else IfTypeThenLambda(>>, operation, cType, func) \
+                    IfTypeThenLambda(&, "AND", operation, cType, func) \
+                    else IfTypeThenLambda(|, "OR", operation, cType, func) \
+                    else IfTypeThenLambda(^, "XOR", operation, cType, func) \
+                    else IfTypeThenLambda(<<, "LEFTSHIFT", operation, cType, func) \
+                    else IfTypeThenLambda(>>, "RIGHTSHIFT", operation, cType, func) \
                     else throw Pothos::InvalidArgumentException("Invalid operation", operation); \
                     break; \
                 case ScalarBlockType::LOGICAL: \
-                    IfTypeThenLambda(&&, operation, cType, func) \
-                    else IfTypeThenLambda(||, operation, cType, func) \
+                    IfTypeThenLambda(&&, "AND", operation, cType, func) \
+                    else IfTypeThenLambda(||, "OR", operation, cType, func) \
                     else throw Pothos::InvalidArgumentException("Invalid operation", operation); \
                     break; \
                 default: \
@@ -148,6 +151,7 @@ static Pothos::Block* makeScalarOpBlock(
                            device, \
                            func, \
                            dtype, \
+                           ((ScalarBlockType::COMPARATOR == blockType) || (ScalarBlockType::LOGICAL == blockType)) ? Int8DType : dtype, \
                            scalarObject.convert<cType>(), \
                            allowZeroScalar); \
         }
@@ -159,26 +163,173 @@ static Pothos::Block* makeScalarOpBlock(
     IfTypeDeclareFactory(std::uint16_t)
     IfTypeDeclareFactory(std::uint32_t)
     IfTypeDeclareFactory(std::uint64_t)
-    IfTypeDeclareFactory(float)
-    IfTypeDeclareFactory(double)
-    IfTypeDeclareFactory(std::complex<float>)
-    IfTypeDeclareFactory(std::complex<double>)
+
+    if((ScalarBlockType::ARITHMETIC == blockType) || (ScalarBlockType::COMPARATOR == blockType))
+    {
+        IfTypeDeclareFactory(float)
+        IfTypeDeclareFactory(double)
+    }
+    if(ScalarBlockType::ARITHMETIC == blockType)
+    {
+        IfTypeDeclareFactory(std::complex<float>)
+        IfTypeDeclareFactory(std::complex<double>)
+    }
 
     throw Pothos::InvalidArgumentException("Invalid type", dtype.name());
 }
 
+//
+// Block registries
+//
+
+/*
+ * |PothosDoc Scalar Arithmetic
+ *
+ * Perform the specified arithmetic operation on the input stream, using
+ * the specified scalar value as the second operand. The result is returned
+ * in the output stream.
+ *
+ * |category /ArrayFire/Scalar Operations
+ * |keywords scalar add subtract multiply divide modulus
+ * |factory /arrayfire/scalar/arithmetic(device,operation,dtype,scalar)
+ * |setter setScalar(scalar)
+ *
+ * |param device[Device] ArrayFire device to use.
+ * |default "Auto"
+ *
+ * |param operation[Operation] The arithmetic operation to perform.
+ * |widget ComboBox(editable=false)
+ * |option [Add] "ADD"
+ * |option [Subtract] "SUBTRACT"
+ * |option [Multiply] "MULTIPLY"
+ * |option [Divide] "DIVIDE"
+ * |option [Modulus] "MODULUS"
+ * |default "ADD"
+ * |preview enable
+ *
+ * |param dtype[Data Type] The output's data type.
+ * |widget DTypeChooser(int16=1,int32=1,int64=1,uint=1,float=1,cfloat=1)
+ * |default "float64"
+ * |preview disable
+ *
+ * |param scalar[Scalar] The scalar value used in the operation.
+ * |widget LineEdit()
+ * |default 0
+ * |preview enable
+ */
 static Pothos::BlockRegistry registerScalarArithmetic(
     "/arrayfire/scalar/arithmetic",
     Pothos::Callable(&makeScalarOpBlock).bind(ScalarBlockType::ARITHMETIC, 0));
 
+/*
+ * |PothosDoc Scalar Comparator
+ *
+ * Perform the specified comparison between each element in the input stream
+ * and the given scalar, returning the result in an output stream of type <b>int8</b>,
+ * where <b>0</b> corresponds to false and <b>1</b> corresponds to true.
+ *
+ * |category /ArrayFire/Scalar Operations
+ * |keywords scalar less greater not equal
+ * |factory /arrayfire/scalar/comparator(device,operation,dtype,scalar)
+ * |setter setScalar(scalar)
+ *
+ * |param device[Device] ArrayFire device to use.
+ * |default "Auto"
+ *
+ * |param operation[Operation] The comparison to perform.
+ * |widget ComboBox(editable=false)
+ * |option [>] ">"
+ * |option [>=] ">="
+ * |option [<] "<"
+ * |option [<=] "<="
+ * |option [==] "=="
+ * |option [!=] "!="
+ * |default ">"
+ * |preview enable
+ *
+ * |param dtype[Data Type] The output's data type.
+ * |widget DTypeChooser(int16=1,int32=1,int64=1,uint=1,float=1)
+ * |default "float64"
+ * |preview disable
+ *
+ * |param scalar[Scalar] The scalar value used in the operation.
+ * |widget LineEdit()
+ * |default 0
+ * |preview enable
+ */
 static Pothos::BlockRegistry registerScalarComparator(
     "/arrayfire/scalar/comparator",
     Pothos::Callable(&makeScalarOpBlock).bind(ScalarBlockType::COMPARATOR, 0));
 
+/*
+ * |PothosDoc Scalar Bitwise
+ *
+ * Perform the specified bitwise operation between each element in the input stream
+ * and the given scalar, returning the result in an output stream.
+ *
+ * |category /ArrayFire/Scalar Operations
+ * |keywords scalar and or left right shift
+ * |factory /arrayfire/scalar/bitwise(device,operation,dtype,scalar)
+ * |setter setScalar(scalar)
+ *
+ * |param device[Device] ArrayFire device to use.
+ * |default "Auto"
+ *
+ * |param operation[Operation] The bitwise operation to perform.
+ * |widget ComboBox(editable=false)
+ * |option [And] "AND"
+ * |option [Or] "OR"
+ * |option [Left Shift] "LEFTSHIFT"
+ * |option [Right Shift] "RIGHTSHIFT"
+ * |default "AND"
+ * |preview enable
+ *
+ * |param dtype[Data Type] The output's data type.
+ * |widget DTypeChooser(int16=1,int32=1,int64=1,uint=1)
+ * |default "uint64"
+ * |preview disable
+ *
+ * |param scalar[Scalar] The scalar value used in the operation.
+ * |widget SpinBox()
+ * |default 0
+ * |preview enable
+ */
 static Pothos::BlockRegistry registerScalarBitwise(
     "/arrayfire/scalar/bitwise",
     Pothos::Callable(&makeScalarOpBlock).bind(ScalarBlockType::BITWISE, 0));
 
+/*
+ * |PothosDoc Scalar Logical
+ *
+ * Perform the specified logical operation between each element in the input stream
+ * and the given scalar, returning the result in an output stream of type <b>int8</b>,
+ * where <b>0</b> corresponds to false and <b>1</b> corresponds to true.
+ *
+ * |category /ArrayFire/Scalar Operations
+ * |keywords scalar and or
+ * |factory /arrayfire/scalar/logical(device,operation,dtype,scalar)
+ * |setter setScalar(scalar)
+ *
+ * |param device[Device] ArrayFire device to use.
+ * |default "Auto"
+ *
+ * |param operation[Operation] The bitwise operation to perform.
+ * |widget ComboBox(editable=false)
+ * |option [And] "AND"
+ * |option [Or] "OR"
+ * |default "AND"
+ * |preview enable
+ *
+ * |param dtype[Data Type] The output's data type.
+ * |widget DTypeChooser(int16=1,int32=1,int64=1,uint=1)
+ * |default "uint64"
+ * |preview disable
+ *
+ * |param scalar[Scalar] The scalar value used in the operation.
+ * |widget SpinBox()
+ * |default 0
+ * |preview enable
+ */
 static Pothos::BlockRegistry registerScalarLogical(
     "/arrayfire/scalar/logical",
     Pothos::Callable(&makeScalarOpBlock).bind(ScalarBlockType::LOGICAL, 0));
