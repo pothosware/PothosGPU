04c055a33827cf841f5fa32539a1382d60ea0737
Nicholas Corgan <n.corgan@gmail.com>
2020-04-26 10:55:48 -0500

    Added floating-point corner cases to cast block test

--- Testing/CastBlockTest.cpp
+++ Testing/CastBlockTest.cpp
@@ -11,9 +11,43 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
+#include <limits>
 #include <string>
 #include <typeinfo>
 #include <vector>
+
+static void addCornerCases(
+    const std::string& type1,
+    const std::string& type2,
+    Pothos::BufferChunk& inputBuffer)
+{
+    POTHOS_TEST_GE(inputBuffer.elements(), 9);
+
+    if((("float32" == type1) || ("complex_float32" == type1)) && Pothos::DType(type2).isFloat())
+    {
+        inputBuffer.as<float*>()[0] = std::numeric_limits<float>::min();
+        inputBuffer.as<float*>()[1] = std::numeric_limits<float>::max();
+        inputBuffer.as<float*>()[2] = std::numeric_limits<float>::infinity();
+        inputBuffer.as<float*>()[3] = -std::numeric_limits<float>::infinity();
+        inputBuffer.as<float*>()[4] = std::numeric_limits<float>::lowest();
+        inputBuffer.as<float*>()[5] = std::numeric_limits<float>::epsilon();
+        inputBuffer.as<float*>()[6] = std::nexttoward(std::numeric_limits<float>::min(), 0.0f);
+        inputBuffer.as<float*>()[7] = std::nexttoward(std::numeric_limits<float>::max(), 0.0f);
+        inputBuffer.as<float*>()[8] = std::numeric_limits<float>::denorm_min();
+    }
+    else if(("float64" == type1) && ("complex_float64" == type2))
+    {
+        inputBuffer.as<double*>()[0] = std::numeric_limits<double>::min();
+        inputBuffer.as<double*>()[1] = std::numeric_limits<double>::max();
+        inputBuffer.as<double*>()[2] = std::numeric_limits<double>::infinity();
+        inputBuffer.as<double*>()[3] = -std::numeric_limits<double>::infinity();
+        inputBuffer.as<double*>()[4] = std::numeric_limits<double>::lowest();
+        inputBuffer.as<double*>()[5] = std::numeric_limits<double>::epsilon();
+        inputBuffer.as<double*>()[6] = std::nexttoward(std::numeric_limits<double>::min(), 0.0f);
+        inputBuffer.as<double*>()[7] = std::nexttoward(std::numeric_limits<double>::max(), 0.0f);
+        inputBuffer.as<double*>()[8] = std::numeric_limits<double>::denorm_min();
+    }
+}
 
 static void testCastBlock(
     const std::string& type1,
@@ -50,6 +84,7 @@ static void testCastBlock(
                                    outputDType);
 
         auto testInputs = AFTests::getTestInputs(inputDType.name());
+        addCornerCases(type1, type2, testInputs);
 
         auto feederSource = Pothos::BlockRegistry::make(
                                 "/blocks/feeder_source",
