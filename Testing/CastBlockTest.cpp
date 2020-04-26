921f3fb9d55a1202912d4ec9360373c9c8e3dd89
Nicholas Corgan <n.corgan@gmail.com>
2020-04-26 10:29:09 -0500

    Added output verification for cast output, could be better

--- Testing/CastBlockTest.cpp
+++ Testing/CastBlockTest.cpp
@@ -19,9 +19,9 @@ static void testCastBlock(
     const std::string& type1,
     const std::string& type2)
 {
-    static constexpr const char* blockRegistryPath = "/arrayfire/array/cast";
+    static constexpr const char* afCastRegistryPath = "/arrayfire/array/cast";
 
-    std::cout << "Testing " << blockRegistryPath
+    std::cout << "Testing " << afCastRegistryPath
               << " (types: " << type1 << " -> " << type2 << ")" << std::endl;
 
     Pothos::DType inputDType(type1);
@@ -31,7 +31,7 @@ static void testCastBlock(
     {
         POTHOS_TEST_THROWS(
             Pothos::BlockRegistry::make(
-                blockRegistryPath,
+                afCastRegistryPath,
                 "Auto",
                 type1,
                 type2),
@@ -39,11 +39,15 @@ static void testCastBlock(
     }
     else
     {
-        auto block = Pothos::BlockRegistry::make(
-                         blockRegistryPath,
-                         "Auto",
-                         type1,
-                         type2);
+        auto afCast = Pothos::BlockRegistry::make(
+                          afCastRegistryPath,
+                          "Auto",
+                          type1,
+                          type2);
+
+        auto pothosConverter = Pothos::BlockRegistry::make(
+                                   "/blocks/converter",
+                                   outputDType);
 
         auto testInputs = AFTests::getTestInputs(inputDType.name());
 
@@ -52,25 +56,36 @@ static void testCastBlock(
                                 inputDType);
         feederSource.call("feedBuffer", testInputs);
 
-        auto collectorSink = Pothos::BlockRegistry::make(
-                                 "/blocks/collector_sink",
-                                 outputDType);
+        auto afCollectorSink = Pothos::BlockRegistry::make(
+                                   "/blocks/collector_sink",
+                                   outputDType);
+        auto pothosCollectorSink = Pothos::BlockRegistry::make(
+                                       "/blocks/collector_sink",
+                                       outputDType);
 
         // Execute the topology.
         {
             Pothos::Topology topology;
-            topology.connect(feederSource, 0, block, 0);
-            topology.connect(block, 0, collectorSink, 0);
+            topology.connect(feederSource, 0, afCast, 0);
+            topology.connect(afCast, 0, afCollectorSink, 0);
+
+            topology.connect(feederSource, 0, pothosConverter, 0);
+            topology.connect(pothosConverter, 0, pothosCollectorSink, 0);
 
             topology.commit();
             POTHOS_TEST_TRUE(topology.waitInactive(0.05));
         }
 
-        // TODO: output verification
-        auto output = collectorSink.call<Pothos::BufferChunk>("getBuffer");
+        // This block is meant to be a faster version of Pothos's converter
+        // block, so we need to make sure the outputs match.
+        auto afOutput = afCollectorSink.call<Pothos::BufferChunk>("getBuffer");
+        auto pothosOutput = pothosCollectorSink.call<Pothos::BufferChunk>("getBuffer");
         POTHOS_TEST_EQUAL(
             testInputs.elements(),
-            output.elements());
+            afOutput.elements());
+        AFTests::testBufferChunk(
+            pothosOutput,
+            afOutput);
     }
 }
 
