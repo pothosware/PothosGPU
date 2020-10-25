59312fffbecacf1e5795bcd720060a9e8bce66c8
Nicholas Corgan <n.corgan@gmail.com>
2020-10-25 16:43:21 -0500

    Enough tests brought over to show what needs fixing

--- Testing/TestUtility.hpp
+++ Testing/TestUtility.hpp
@@ -13,6 +13,7 @@
 #include <algorithm>
 #include <complex>
 #include <cstring>
+#include <iostream>
 #include <limits>
 #include <random>
 #include <sstream>
@@ -159,6 +160,8 @@ template <typename T>
 static std::vector<T> linspace(T a, T b, size_t N)
 {
     T h = (b - a) / static_cast<T>(N-1);
+    std::cout << a << " " << b << " " << N << " " << h << std::endl;
+
     std::vector<T> xs(N);
     typename std::vector<T>::iterator x;
     T val;
