04b11c62b5b63bcf8146ca8f97c3c8a7f82153f1
Nicholas Corgan <n.corgan@gmail.com>
2020-10-28 08:06:54 -0500

    Added bitwise-not fallback implementation

--- Source/BitwiseNot.cpp
+++ Source/BitwiseNot.cpp
@@ -2,12 +2,15 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 #include "OneToOneBlock.hpp"
+#include "Utility.hpp"
 
-#include <Pothos/Callable.hpp>
-#include <Pothos/Object.hpp>
+#include <Pothos/Exception.hpp>
 #include <Pothos/Framework.hpp>
+#include <Pothos/Object.hpp>
 
 #include <arrayfire.h>
+
+#include <numeric>
 
 #if AF_API_VERSION >= 38
 
@@ -18,7 +21,52 @@ static af::array afNot(const af::array& afArray)
 
 #else
 
+// We should have caught this by this point.
+class DTypeAssertionViolationException: public Pothos::AssertionViolationException
+{
+    public:
+        DTypeAssertionViolationException(af::dtype dtype):
+            Pothos::AssertionViolationException(
+                "We didn't catch an invalid DType: " +
+                Pothos::Object(dtype).convert<Pothos::DType>().toString())
+        {}
 
+        virtual ~DTypeAssertionViolationException() = default;
+};
+
+namespace detail
+{
+    // https://en.wikipedia.org/wiki/Bitwise_operation#NOT
+
+    template <typename T>
+    EnableIfInteger<T, af::array> afNot(const af::array& afArray)
+    {
+        return -afArray - 1;
+    }
+
+    template <typename T>
+    EnableIfUnsignedInt<T, af::array> afNot(const af::array& afArray)
+    {
+        return std::numeric_limits<T>::max() - afArray;
+    }
+}
+
+static af::array afNot(const af::array& afArray)
+{
+    switch(afArray.type())
+    {
+        case s16: return detail::afNot<std::int16_t>(afArray);
+        case s32: return detail::afNot<std::int32_t>(afArray);
+        case s64: return detail::afNot<std::int64_t>(afArray);
+        case u8:  return detail::afNot<std::uint8_t>(afArray);
+        case u16: return detail::afNot<std::uint16_t>(afArray);
+        case u32: return detail::afNot<std::uint32_t>(afArray);
+        case u64: return detail::afNot<std::uint64_t>(afArray);
+
+        default:
+            throw DTypeAssertionViolationException(afArray.type());
+    }
+}
 
 #endif
 
